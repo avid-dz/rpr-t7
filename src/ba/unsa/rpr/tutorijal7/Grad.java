56478306dc6419b5f8b7d00f5734c3b9bc52ac6d
Dzavid <dbrdar1@etf.unsa.ba>
2018-12-05 15:05:17 +0100

    Priprema uradjena

---
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -0,0 +1,30 @@
+package ba.unsa.rpr.tutorijal7;
+
+public class Grad {
+
+    private String naziv = "";
+    private int brojStanovnika = 0;
+    private double[] temperature = new double[1000];
+
+    public Grad() {
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+    public double[] getTemperature() {
+        return temperature;
+    }
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+}
---
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -0,0 +1,18 @@
+package ba.unsa.rpr.tutorijal7;
+
+import java.util.ArrayList;
+
+public class UN {
+
+    private ArrayList<Drzava> listaDrzava = new ArrayList<>();
+
+    public UN() {
+    }
+
+    public ArrayList<Drzava> getListaDrzava() {
+        return listaDrzava;
+    }
+    public void setListaDrzava(ArrayList<Drzava> listaDrzava) {
+        this.listaDrzava = listaDrzava;
+    }
+}
