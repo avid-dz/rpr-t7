7aa0b576b4ceb07ece665f35c75c358e3b6df05d
Dzavid <dbrdar1@etf.unsa.ba>
2018-12-05 15:21:37 +0100

    Zadatak 1 zapocet

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,7 +1,23 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Tutorijal {
 
     public static void main(String[] args) {
+
+    }
+
+    public static void ucitajGradove() {
+        Scanner ulazniTok = null;
+        try {
+            ulazniTok = new Scanner(new FileReader("mjerenja.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka \"mjerenja.txt\" ne postoji ili se ne moze otvoriti.");
+        } finally {
+            ulazniTok.close();
+        }
     }
 }
